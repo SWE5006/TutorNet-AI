f4ff2601bdbdc3058ededa2362e16e3a2b8767da
Fang Ziyue <e0838388@u.nus.edu>
2025-11-08 10:52:56 +0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -42,7 +42,7 @@ app = FastAPI(
 #Add health check
 @app.get("/healthz")
 def health_check():
-    return {"status": "ok"}
+    return {"status": "ok-cicd-demo"}
 
 # Add error handling middleware
 @app.exception_handler(BaseApplicationError)
